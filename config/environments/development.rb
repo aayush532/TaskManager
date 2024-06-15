611ef8405947674ad604887966608d5d18940594
aayush <aayushmishra532@gmail.com>
2024-06-15 13:35:52 +0530

    changed in destroy button

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,16 +75,6 @@ Rails.application.configure do
   config.action_controller.raise_on_missing_callback_actions = true
 
   config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   address: 'smtp-relay.brevo.com',
-  #   port: 587,
-  #   domain: 'smtp-relay.brevo.com',
-  #   user_name: '76afc4001@smtp-brevo.com',
-  #   password: 'LpWBSDFtvNGHaX8j',
-  #   authentication: 'plain',
-  #   enable_starttls_auto: true
-  # }
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
